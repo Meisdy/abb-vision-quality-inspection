b948b4288dec98a54e96afc63acb6be9b6441827
BOTSpencer <sandy.meister@gmx.ch>
2025-09-30 11:52:52 +0200

    Implemented the Robot Comm functions as a class and its own functions.

--- abb_robot_comm.py.py
+++ abb_robot_comm.py.py
@@ -1,6 +1,58 @@
 import socket
 
 
+class RobotComm:
+    def __init__(self, ip, port=5000):
+        self.ip = ip
+        self.port = port
+        self.socket = None
+        self.client_socket = None
+        self.client_ip = None
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen()
+        print("Looking for client")
+
+        (self.client_socket, self.client_ip) = self.socket.accept()
+        print(f"Robot at address {self.client_ip} connected.")
+        print("If you would like to end the program, enter 'quit'.")
+
+    def disconnect(self):
+        if self.client_socket is not None:
+            self.client_socket.close()
+
+    def communicate(self, message=None):
+        while True:
+            if message is None:
+                user_input = input("Please type your message: ")
+            else:
+                user_input = message
+
+            if user_input.lower() == "quit":
+                self.client_socket.send(user_input.encode("UTF-8"))
+                print("Goodbye!")
+                # wait for answer and print in terminal
+                if self.client_socket:
+                    client_message = self.client_socket.recv(4094)
+                    client_message = client_message.decode("latin-1")
+                    print("The received message is:", client_message)
+                    return client_message
+
+                break
+            else:
+                self.client_socket.send(user_input.encode("UTF-8"))
+                print(f"Message sent to client: {user_input}")
+                # Wait for answer and print in terminal
+                if self.client_socket:
+                    print("The received message is:")
+                    client_message = self.client_socket.recv(4094)
+                    client_message = client_message.decode("latin-1")
+                    print("!!", client_message)
+                    return client_message
+
+
 def connect_rob(ip):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Set socket listening port. To find the robot address use cmd and type ipconfig in the terminal, there you see
