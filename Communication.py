94ad970e4cf1b1da738edc81759ed576a3642dd6
BOTSpencer <sandy.meister@gmx.ch>
2025-09-30 10:55:11 +0200

    Created a Communication file. All Robot Comm should be in this file as a module.

---
+++ Communication.py
@@ -0,0 +1,56 @@
+import socket
+
+
+def connect_rob(ip):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Set socket listening port. To find the robot address use cmd and type ipconfig in the terminal, there you see
+    # the ip of the robot. you may have to open the port in the firewall as well. Note that the ip address is
+    # 127.0.0.1 if you run via RobotStudio
+    server_socket.bind((ip, 5000))
+
+    # Set up the server
+    # listen to incoming client connection
+    server_socket.listen()
+    print("Looking for client")
+
+    # accept and store incoming socket connection
+    (client_socket, client_ip) = server_socket.accept()
+    print(f"Robot at address {client_ip} connected.")
+    print("If you would like to end the program, enter 'quit'.")
+    return client_socket
+
+
+def disconnect_rob(client_socket):
+    client_socket.close()
+
+
+def communicate_rob(message, client_socket):
+    while True:
+        if message is None:
+            UserInput = input("Please type your message: ")
+        else:
+            UserInput = message
+
+        if UserInput.lower() == "quit":
+            server_message = UserInput
+            client_socket.send(server_message.encode("UTF-8"))
+            print("Goodbye!")
+            # wait for answer and print in terminal
+            if client_socket.recv:
+                client_message = client_socket.recv(4094)
+                client_message = client_message.decode("latin-1")
+                print("The received message is:", client_message)
+                return client_message
+
+            break
+
+        else:
+            client_socket.send(UserInput.encode("UTF-8"))
+            print(f"Message sent to client: {message}")
+            # Wait for answer and print in terminal
+            if client_socket.recv:
+                print("The received message is:")
+                client_message = client_socket.recv(4094)
+                client_message = client_message.decode("latin-1")
+                print("!!", client_message)
+                return client_message
