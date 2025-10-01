20940f0801dc9cf702f2df9d16b8c84529f405b3
BOTSpencer <sandy.meister@gmx.ch>
2025-10-01 11:46:05 +0200

    Refined the Testing code in main for Robot Com.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ os.environ["PYLON_CAMEMU"] = "3"
 # Constants
 USE_CAMERA = False
 USE_ROBOT = False
-IMAGE_NAME = 'Image6.jpg'
+#IMAGE_NAME = 'Image6.jpg'
 IP_ABB_ROBOT = '192.168.125.202'
 Picture = []
 
@@ -25,16 +25,22 @@ exitCode = 0
 def main():
     if USE_ROBOT:
 
-        print("LOG: Using Robot. Setting up TCP Server now")
+        # Testing TCP Server with ABB Robot
         Robot = abb_robot_comm.RobotComm(IP_ABB_ROBOT)
         Robot.connect()
-        Robot.communicate("Hello from Python")
+        answer = Robot.communicate("Vision System Ready")
+        if answer == "Robot ready":
+            print("LOG: Robot is ready. Proceeding with vision system tasks.")
+        else:
+            print("LOG: Robot did not respond as expected. Check connection.")
+            sys.exit(1)
+
+        # Wait for user input to disconnect
+        user_input = input("Press Enter to disconnect from the robot...")
         Robot.disconnect()
 
-        print("LOG: Using Robot. Setting up TCP Server now")
-
     else:
-        print("LOG: Not using Robot. Skipping TCP Server setup")
+        sys.exit(1)
 
 
 # Press the green button in the gutter to run the script.
