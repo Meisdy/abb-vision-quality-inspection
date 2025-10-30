24a93a4dbc8a24a44bb38521996dcfc7c1901d7d
BOTSpencer <sandy.meister@gmx.ch>
2025-10-30 15:58:54 +0100

    Changed file so one can take images with space. Exit with ESC

--- Basler.py
+++ Basler.py
@@ -47,26 +47,29 @@ def grab_images(cameras, converter):
 
     while cameras.IsGrabbing():
         grabResult1 = cameras[0].RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-        # grabResult2 = cameras[1].RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
         if grabResult1.GrabSucceeded():
             frame_counter += 1
             image1 = converter.Convert(grabResult1).GetArray()
 
             cv2.namedWindow('Camera 1 Feed', cv2.WINDOW_NORMAL)
-            # cv2.namedWindow('Camera 2 Feed', cv2.WINDOW_NORMAL)
 
-            # Show each camera feed in its respective window
+            # Show the camera feed
             cv2.imshow('Camera 1 Feed', image1)
-            # cv2.imshow('Camera 2 Feed', image2)
 
-            if cv2.waitKey(1) == 27:  # Press 'ESC' to exit
+            # Wait for key press (1ms delay)
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == 27:  # Press 'ESC' to exit
+                grabResult1.Release()
                 break
-            elif cv2.waitKey(1) == 32:  # Press 'Space' to save the image
+            elif key == 32:  # Press 'Space' to save the image
                 image_counter += 1
                 filename = f"camera1_image_{image_counter}.png"
                 cv2.imwrite(filename, image1)
                 print(f"Image saved as {filename}")
+
+        grabResult1.Release()
 
     cameras.StopGrabbing()
     cv2.destroyAllWindows()
