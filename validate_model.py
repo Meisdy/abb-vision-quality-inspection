a97c74c5376fe5d218ce8a38b62113942f992738
BOTSpencer <sandy.meister@gmx.ch>
2025-11-04 11:16:54 +0100

    Replaced autoencoder code with import from model file

--- validate_model.py
+++ validate_model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import cv2
 import numpy as np
 import os
+from models import Autoencoder
 
 
 # Load images function (same as train.py)
@@ -26,36 +27,6 @@ def load_images(folder):
             img = np.transpose(img, (2, 0, 1))
             images.append(img)
     return np.array(images)
-
-
-# Autoencoder model (same as train.py)
-class Autoencoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(256, 128, 2, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, 2, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 3, 2, stride=2),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
 
 
 # Load trained model
