f08f5c0ed1a667c94ff951a85ab0972775650114
BOTSpencer <sandy.meister@gmx.ch>
2025-11-12 16:14:23 +0100

    Changed IP. Added conversion to raw file before giving into eval. Needs to be tested.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import os
 import cv2
-import torch
+import numpy as np
 import logging
 import abb_robot_comm
 import vision_pipeline
 import ML.classifier_evaluation as ml
-from vision_pipeline import VisionProcessor
 
 os.environ["PYLON_CAMEMU"] = "3"
 
 # Constants
 USE_CAMERA = True
 USE_ROBOT = True
-IP_ABB_ROBOT = '192.168.125.201'
+IP_ABB_ROBOT = '192.168.125.5'
 maxCamerasToUse = 1
 
 
@@ -33,25 +32,32 @@ def setup_logging():
     logging.getLogger().setLevel(logging.INFO)
 
 
-def evaluate_part(Camera, ml_evaluator) -> str:
+def evaluate_part(Camera, ml_evaluator) -> bool:
     logging.info("Starting part evaluation")
 
     # Capture image from camera
     img = Camera.capture_raw()  # BGR, numpy array
+
     if img is None:
         logging.error("Failed to capture image")
         return False
 
+    # Convert BGR -> RGB so preprocessing matches Image.open(...).convert('RGB')
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    rgb_img = np.asarray(rgb_img, dtype=np.uint8)  # ensure dtype    if img is None:
+
     # Classify using the trained model
     logging.info(f"Loaded model with classes: {ml_evaluator.classes}")
-    label, confidence = ml_evaluator.predict_one(img)  # pass raw image, gets processed in method
-    logging.info(f"Classification result: {label} with confidence {confidence:.4f}")
+    label, confidence = ml_evaluator.predict_one(rgb_img)  # pass raw image, gets processed in method
+    logging.info(f"Classification result: {label.upper()} with confidence {confidence:.4f}")
 
-    # Get all classes from the evaluator but incorrect_mix
-    good_classes = [cls for cls in ml_evaluator.classes if cls != 'incorrect_mix']
+    # Determine if part is correct or incorrect
+    if confidence < 0.6:
+        logging.warning("Low confidence in prediction, marking as incorrect mix")
+        label = 'incorrect_mix'
 
     # Return status
-    return 'complete' if label in good_classes else 'part bad'
+    return False if label == 'incorrect_mix' else True
 
 
 def main():
@@ -71,9 +77,11 @@ def main():
                 logging.info("Received evaluate command from robot.")
                 part_status = evaluate_part(Camera, ml_evaluator)
                 if part_status:
-                    Robot.send_message("complete")
+                    Robot.send_message("COMPLETE")
                 else:
-                    Robot.send_message("part bad")
+                    Robot.send_message("BAD_PART")
+
+            logging.info('Client disconnected')
 
     except Exception as e:
         logging.error(f"Error: {e}")
